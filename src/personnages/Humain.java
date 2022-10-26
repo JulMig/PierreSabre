c090f0ed24eef12f177361775e862eed84447758
JulMig <julie.mignotte8@gmail.com>
2022-10-26 14:42:12 +0200

    classe humain
    creation classe humain

---
+++ src/personnages/Humain.java
@@ -0,0 +1,51 @@
+package personnages;
+
+public class Humain {
+	private String nom;
+	private String boisson;
+	private int argent;
+	
+	public Humain(String nom, String boisson, int argent) {
+		this.nom = nom;
+		this.boisson = boisson;
+		this.argent = argent;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public int getArgent() {
+		return argent;
+	}
+
+	public void direBonjour() {
+		parler("Bonjour ! Je m'appelle " + getNom() + " et j'aime boire du " + boisson);
+	}
+	
+	public void boire() {
+		parler("Mmmm, un bon verre de " + boisson + " ! GLOUPS !");
+	}
+	
+	public void acheter(String bien, int prix) {
+		if (prix <= getArgent()){
+			parler("J'ai " + getArgent() + " sous en poche. Je vais pouvoir m'offrir " + bien + " à " + prix + " sous.");
+			perdreArgent(prix);
+		} else {
+			parler("Je n'ai plus que " + getArgent() + " sous en poche. Je ne peux même pas m'offrir " + bien + " à " + prix +" sous.");
+		}
+	}
+	
+	public void gagnerArgent(int gain) {
+		argent += gain;
+	}
+	
+	public void perdreArgent(int perte) {
+		argent -= perte;
+	}
+	
+	private void parler(String texte){
+		System.out.println("(" + getNom() + ") - " + texte);
+		
+	}
+}
